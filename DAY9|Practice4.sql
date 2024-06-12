2541bc8dd2ef0654bb71ba1390b2c394099bdd21
dthuha <thuha.dao11042003@gmail.com>
2024-06-13 05:59:37 +0700

    Create DAY9|Practice4.sql

---
+++ DAY9|Practice4.sql
@@ -0,0 +1,38 @@
+--- Laptop vs. Mobile Viewership [New York Times SQL Interview Question]
+SELECT
+SUM(CASE
+WHEN device_type = 'laptop' THEN 1
+ELSE 0
+END) laptop_views,
+SUM(CASE
+WHEN device_type IN ('tablet', 'phone') THEN 1
+ELSE 0
+END) mobile_views
+FROM viewership;
+--- Triangle Judgement
+SELECT x,y,z,
+CASE
+WHEN x+y>z AND x+z>y AND y+z>x THEN 'Yes' --- dùng dc AND (thể hiện 3 điều kiện đồng thời xảy ra)
+ELSE 'No'
+END triangle
+FROM TRIANGLE
+--- Patient Support Analysis (Part 2) [UnitedHealth SQL Interview Question]
+
+--- Find Customer Referee
+select name from customer where referee_id !=2 or referee_id is null
+--- Make a report showing the number of survivors and non-survivors by passenger class
+select survived,
+SUM(CASE
+WHEN pclass = '1' THEN 1
+ELSE 0
+END) first_class,
+SUM(CASE
+WHEN pclass = '2' THEN 1
+ELSE 0
+END) second_classs,
+SUM(CASE
+WHEN pclass = '3' THEN 1
+ELSE 0
+END) third_class
+from titanic
+GROUP BY SURVIVED
