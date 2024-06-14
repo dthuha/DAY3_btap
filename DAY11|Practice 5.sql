ad39cb09ce93c5bea32d916780edfea2cd01aad6
dthuha <thuha.dao11042003@gmail.com>
2024-06-14 15:15:39 +0700

    Create DAY11|Practice 5.sql

---
+++ DAY11|Practice 5.sql
@@ -0,0 +1,7 @@
+-- EX1: Average Population of Each Continent
+SELECT T2.CONTINENT, FLOOR(AVG(T1.POPULATION))
+FROM CITY AS T1
+INNER JOIN COUNTRY AS T2
+ON T1.COUNTRYCODE=T2.CODE
+GROUP BY T2.CONTINENT
+-- EX2: Signup Activation Rate [TikTok SQL Interview Question]
