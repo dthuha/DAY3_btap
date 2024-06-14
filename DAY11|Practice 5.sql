fc0406eecd1e74da8778c51c2600ffd29b4d95cc
dthuha <thuha.dao11042003@gmail.com>
2024-06-14 21:26:17 +0700

    Update DAY11|Practice 5.sql

--- DAY11|Practice 5.sql
+++ DAY11|Practice 5.sql
@@ -5,3 +5,12 @@ INNER JOIN COUNTRY AS T2
 ON T1.COUNTRYCODE=T2.CODE
 GROUP BY T2.CONTINENT
 -- EX2: Signup Activation Rate [TikTok SQL Interview Question]
+SELECT
+ROUND(CAST(SUM(CASE
+WHEN T1.signup_action = 'Confirmed' THEN 1
+ELSE 0
+END) AS DECIMAL) /COUNT(T1.signup_action), 2) AS activation_rate
+FROM TEXTS AS T1
+INNER JOIN EMAILS AS T2
+ON T1.email_id=T2.email_id
+--
