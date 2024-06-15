942011e1a108cc4fa3b1d5968e53ee2a5af84418
dthuha <thuha.dao11042003@gmail.com>
2024-06-15 22:15:16 +0700

    Update DAY11|Practice 5.sql

--- DAY11|Practice 5.sql
+++ DAY11|Practice 5.sql
@@ -13,4 +13,24 @@ END) AS DECIMAL) /COUNT(T1.signup_action), 2) AS activation_rate
 FROM TEXTS AS T1
 INNER JOIN EMAILS AS T2
 ON T1.email_id=T2.email_id
---
+-- EX3: Sending vs. Opening Snaps [Snapchat SQL Interview Question]
+SELECT t2.age_bucket,
+ROUND(100*
+SUM(CASE 
+WHEN t1.activity_type = 'send' THEN t1.time_spent
+END) :: DECIMAL /
+SUM(CASE 
+WHEN t1.activity_type in ('open','send') THEN t1.time_spent --- ban đầu mình nghĩ là cần cộng case when của từng open và send lại, nhưng kcan, để in ('open', 'send') như kia
+END),2) as send_perc, 
+ROUND(100* 
+SUM(CASE 
+WHEN t1.activity_type = 'open' THEN t1.time_spent
+END) :: DECIMAL /
+SUM(CASE 
+WHEN t1.activity_type in ('open','send') THEN t1.time_spent
+END),2) as open_perc
+FROM activities AS t1
+INNER JOIN age_breakdown AS t2
+ON t1.user_id=t2.user_id
+GROUP BY t2.age_bucket
+-- EX4:
