1eff2284114614310c65c4078dd507f7952830ee
dthuha <thuha.dao11042003@gmail.com>
2024-06-19 17:38:40 +0700

    Update DAY13|Practice 6.sql

--- DAY13|Practice 6.sql
+++ DAY13|Practice 6.sql
@@ -1,4 +1,17 @@
--- EX 1:
+-- EX 1: Duplicate Job Listings [Linkedin SQL Interview Question]
+WITH abc AS 
+(SELECT 
+  company_id, 
+  title, 
+  description, 
+  COUNT(job_id) AS job_count
+FROM job_listings
+GROUP BY company_id, title, description)
+
+SELECT COUNT(company_id)
+FROM abc
+WHERE job_count >1
+
 -- EX2: Highest-Grossing Items [Amazon SQL Interview Question]
 WITH appliance AS
 (SELECT category, product,
@@ -42,7 +55,8 @@ WHERE t2.liked_date is NULL
 ORDER BY t1.page_id
 
 -- EX5:
--- EX6:
+  
+-- EX6: Monthly Transactions I
 select  
 DATE_FORMAT(trans_date, '%Y-%m') AS month,
 country, 
@@ -53,5 +67,67 @@ sum(case when state ='approved 'then amount else 0 end) as approved_total_amount
 from Transactions
 group by month, country
 
--- EX7:
+-- EX7: Product Sales Analysis III
+-- ID, NĂM, CHẤT LG, GIÁ trong năm đầu tiên (= năm MIN) của mỗi sp dc bán
+SELECT 
+product_id, 
+year AS first_year, 
+quantity,
+price
+FROM sales
+WHERE (product_id,year) IN 
+(SELECT 
+  product_id,
+  MIN(year) --- đừng quên MIN, MAX
+FROM sales
+GROUP BY product_id)
 
+-- EX8: Customers Who Bought All Products
+SELECT customer_id FROM customer 
+GROUP BY customer_id
+HAVING COUNT(distinct product_key) = 
+(SELECT COUNT(product_key ) 
+FROM product)
+
+-- EX 9: Employees Whose Manager Left the Company
+SELECT employee_id
+FROM Employees as a
+WHERE manager_id not in (SELECT employee_id FROM employees) and salary < 30000
+ORDER BY employee_id ASC
+
+-- EX 11: Movie Rating
+(SELECT name AS results 
+FROM Users
+JOIN MovieRating
+ON Users.user_id=MovieRating.user_id
+GROUP BY Users.user_id
+ORDER BY COUNT(*) DESC, name ASC
+LIMIT 1)
+
+UNION ALL
+(SELECT results FROM
+(SELECT 
+title AS results,
+AVG(MovieRating.rating) AS trom_via
+FROM Movies
+JOIN MovieRating
+ON Movies.movie_id=MovieRating.movie_id
+WHERE EXTRACT(month from created_at) = 2 AND EXTRACT(year from created_at) = 2020
+GROUP BY (Movies.movie_id)) AS abc
+
+ORDER BY trom_via DESC, results ASC 
+LIMIT 1)
+
+-- EX 12: (bai nay hay) Friend Requests II: Who Has the Most Friends
+WITH ABC AS
+(SELECT requester_id , accepter_id
+FROM RequestAccepted
+UNION ALL
+SELECT accepter_id , requester_id
+FROM RequestAccepted)
+SELECT requester_id AS id,
+COUNT(accepter_id) AS num
+FROM ABC
+GROUP BY requester_id
+ORDER BY num DESC
+LIMIT 1
