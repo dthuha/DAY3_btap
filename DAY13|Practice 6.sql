d9f26349ee492bd83b52c34f3a4cea148e417af8
dthuha <thuha.dao11042003@gmail.com>
2024-06-19 09:57:10 +0700

    Update and rename DAY13|Practice.sql to DAY13|Practice 6.sql

--- DAY13|Practice 6.sql
+++ DAY13|Practice 6.sql
@@ -42,4 +42,16 @@ WHERE t2.liked_date is NULL
 ORDER BY t1.page_id
 
 -- EX5:
+-- EX6:
+select  
+DATE_FORMAT(trans_date, '%Y-%m') AS month,
+country, 
+count(*) as  trans_count ,
+sum(case when state ='approved 'then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state ='approved 'then amount else 0 end) as approved_total_amount 
+from Transactions
+group by month, country
+
+-- EX7:
 
